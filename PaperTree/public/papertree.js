de366d35f6f0a5212de722a5d389394d91b4a3a5
BBB-gm <84646915+BBB-gm@users.noreply.github.com>
2026-03-22 19:31:11 -0400

    Editor functionality implementation

--- PaperTree/public/papertree.js
+++ PaperTree/public/papertree.js
@@ -2,8 +2,16 @@ const titleHeader = document.getElementById("title");
 const bodyParagraph = document.getElementById("body");
 const linkDiv = document.getElementById("linkdiv");
 const url = window.location.origin + "/papertree/node";
+const editDiv = document.getElementById("edit");
+const displayDiv = document.getElementById("display");
+const editButton = document.getElementById("editButton");
+const titleEditor = document.getElementById("titleEditor");
+const bodyEditor = document.getElementById("bodyEditor");
 
+var isEditing = false;
 let pageNumber = parseInt(window.location.search.substring(1));
+
+swapModes(isEditing);
 
 if(isNaN(pageNumber)){
     console.log("hey")
@@ -24,7 +32,11 @@ fetch(url, {
     .then((json)=>{
     
     titleHeader.innerText = json.name;
+    titleEditor.value = json.name;
+
     bodyParagraph.innerText = json.body;
+    bodyEditor.innerText = json.body;
+
     json.links.forEach(element => {
         console.log(element);
         let newLink = document.createElement("a");
@@ -36,3 +48,18 @@ fetch(url, {
         linkDiv.appendChild(newLink);
     });
 })
+
+editButton.addEventListener("click",e=>{
+    swapModes(!isEditing);
+});
+
+function swapModes(isEditingNew){
+    if(isEditingNew){
+        editDiv.style.display = "flex";
+        displayDiv.style.display = "none";
+    } else {
+        editDiv.style.display = "none";
+        displayDiv.style.display = "flex";
+    }
+    isEditing = isEditingNew;
+}
